6474f8de0209052654fd0532b4f6831356bb1118
Quality0610 <kzktcy0610@gmail.com>
2021-12-19 21:01:55 +0900

    実験

--- sample1/src/vars.rs
+++ sample1/src/vars.rs
@@ -22,4 +22,29 @@ pub fn run() {
     let i3: i64 = 2;
     println!("Memory address of i2 is {:p}", &i2);
     println!("Memory address of i3 is {:p}", &i3);
+
+    let y = 2;
+    println!("Memory address of y is {:p}", &y);
+    let y = y + 1;
+    println!("Memory address of y is {:p}", &y);
+    let y = y * 2;
+    println!("Memory address of y is {:p}", &y);
+    println!("value of y is {}", y);
+    {
+        let y = 0;
+        println!("value of y is {}", y);
+    }
+    println!("value of y is {}", y);
+
+    let t1 = (500, 6.4, "dummy");
+    let (_x, _y, _z) = t1;
+    println!("value of t1 is {} {} {}", t1.0, t1.1, t1.2);
+
+    let mut t2 = ((0, 1), (2, 3));
+    println!("{:?}", t2);
+    let ((ref mut x1_ptr, ref mut y1_ptr), _) = t2;
+    *x1_ptr = 5;
+    *y1_ptr = -5;
+    // タプルの出力には?が必要
+    println!("{:?}", t2);
 }
